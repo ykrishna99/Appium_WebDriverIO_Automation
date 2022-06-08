142ea292d8dc8e07c8f6589fa4fdf14f91aabe04
Krishna99 <72746140+ykrishna99@users.noreply.github.com>
2022-06-08 12:18:58 +0530

    first commit

---
+++ test/specs/find_elements.spec.js
@@ -0,0 +1,29 @@
+//Author - Krishna Rao
+
+//Selectors in WebDriverIO
+describe('Finding Elements using Selectors', () => {
+
+    //Using Class Selector
+    it('class selector test', async () => {
+        const apiDemos = await $('android.widget.TextView')
+        await expect(apiDemos).toHaveText('API Demos')
+    })
+
+    //Using Accessibility Id Selector
+    it('accessibility id selector test', async () => {
+        const app = await $('~App')
+        await app.click()
+
+        const actionBar = await $('~Action Bar')
+        await expect(actionBar).toBeDisplayed()
+    })
+
+    //Using XPath Selector
+    it('xpath selector test', async () => {
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+        await $('//android.widget.TextView[@text="Command two"]').click()
+        const messageElem = await $('android.widget.TextView')
+        await expect(await messageElem.getText()).toEqual('You selected: 1 , Command two')
+    })
+})
